8c3a01d8bdfb4f9592883e53dec8dca81a9718ba
Rafiarifp <rafi140906@gmail.com>
2026-04-20 09:14:15 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,50 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 2
+ *   Hari dan Tanggal    : Senin, 20 April 2026
+ *   Nama (NIM)          : Rafi Arif Putra (13224033)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Fungsi yang memulihkan nilai tidak valid (-1) pada array berdasarkan beberapa instruksi
+ * 
+ */
 
-int main(void) {
-    return 0;
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int n; // Deklarasi n sebagai jumlah bilangan
+    scanf("%d", &n);
+    int arr[n];
+
+    // Iterasi for loop untuk bilangan dari user input sebanyak n (jumlah bilangan)
+    for (int i=0; i<n; i++){
+        scanf("%d", &arr[i]); // Bilangan disimpan sebagai data pada array ke-i
+    }
+
+    for (int i = 0; i < n; i++){
+        int truth = 1;
+        if (arr[i] == -1){
+            for(int j = 1; j < n; j++){
+                if (arr[i-j] != -1 && truth == 1){
+                    for (int k = 1; k < n; k++){
+                        if (arr[i+k] != -1 && truth == 1){
+                            int p = arr[i-j];
+                            int n = arr[i+k];
+                            arr[i] = floor((p+n)/2);
+                            truth = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++){
+        sum += arr[i];
+    }
+    printf("RECOVERED");
+    for (int i = 0; i < n; i++){
+        printf(" %d", arr[i]);
+    }
+    printf("\nMAX_SUM %d", sum);
 }
